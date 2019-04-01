d7a4afb678b24bb6296c0159a0750d9dba1e2ddd
erin-koen <erin.koen@gmail.com>
2019-04-01 14:03:20 -0400

    followed luiz through guided lecture

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
 
-const db = require('./database/dbConfig.js');
-const Users = require('./users/users-model.js');
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
 
 const server = express();
 
@@ -11,12 +12,17 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
 });
 
-server.post('/api/register', (req, res) => {
+server.post("/api/register", (req, res) => {
   let user = req.body;
+  //creating a hash of the password
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  //overwriting the password that's been passed with the hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -27,16 +33,17 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', (req, res) => {
+server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      //check the password guess against the database
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
@@ -44,13 +51,48 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+// middleware - restricted = authentication, only = authorization
+server.get("/api/users", restricted, only("erin"), (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(res, req, next) {
+  const { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).send("YOU SHALL NOT PASS");
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(401).json({ message: "please provide credentials" });
+  }
+}
+
+// getting through restrict means that username and password are valid
+// only needs to check the username that has made it this far against whatever was passed into only
+// needs to return the piece of middleware, because it's a function that's been called on an argument - i don't quite get this
+
+function only(name) {
+  return function(res, req, next) {
+    if (req.headers.username === name) {
+      next();
+    } else {
+      res.status(403).send("YOU SHALL NOT PASS");
+    }
+  };
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
